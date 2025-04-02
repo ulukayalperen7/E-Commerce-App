270845d01d7b8eb44634bce63dc15fa069c7945c
Alperen Ulukaya <alperenulukaya07@gmail.com>
2025-04-02 20:06:35 +0300

    updated banner component

--- ecommerce-app/src/app/shared/components/banner/banner.component.ts
+++ ecommerce-app/src/app/shared/components/banner/banner.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
-  standalone: false,
+  standalone : false,
   templateUrl: './banner.component.html',
-  styleUrl: './banner.component.scss'
+  styleUrls: ['./banner.component.scss']
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit, OnDestroy {
+  items = [
+    { image: 'path/to/image1.jpg', altText: 'Image 1' },
+    { image: 'path/to/image2.jpg', altText: 'Image 2' },
+    { image: 'path/to/image3.jpg', altText: 'Image 3' }
+  ];
 
+  currentIndex = 0;
+  interval: any;
+
+  ngOnInit() { 
+    this.startAutoSlide();
+  }
+
+  ngOnDestroy() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  startAutoSlide() {
+    this.interval = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.items.length;
+    }, 3000);
+  }
 }
