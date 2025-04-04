da235adc6ae3e1f6bd6aa0e4e10a4efeeeffb58b
Alperen Ulukaya <alperenulukaya07@gmail.com>
2025-04-04 23:09:04 +0300

    fixed navbar elements working correctly | visitor module root level : lazy loading

--- ecommerce-app/src/app/app-routing.module.ts
+++ ecommerce-app/src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { 
+    path: '',  //  visitor module is on the root level (lazyloading )
+    loadChildren: () => import('./features/visitor/visitor.module').then(m => m.VisitorModule) 
+  },
+  { 
     path: 'customer', 
     loadChildren: () => import('./features/customer/customer.module').then(m => m.CustomerModule) 
   },
