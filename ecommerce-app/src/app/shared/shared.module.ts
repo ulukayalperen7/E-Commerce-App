123f97cb2d309b575cf52785a2997ef81a631b6a
Alperen Ulukaya <alperenulukaya07@gmail.com>
2025-05-02 23:02:20 +0300

    deleted shared-components and categories

--- ecommerce-app/src/app/shared/shared.module.ts
+++ ecommerce-app/src/app/shared/shared.module.ts
@@ -3,19 +3,16 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { RouterModule } from '@angular/router';
-import { CategoryNavComponent } from './categories/category-nav/category-nav.component';
 
 @NgModule({
   declarations: [
     FooterComponent,
     BannerComponent,
-    CategoryNavComponent,
   ],
   imports: [CommonModule,RouterModule],
   exports: [ 
     FooterComponent,
     BannerComponent,
-    CategoryNavComponent
   ]
 })
 export class SharedModule { }
