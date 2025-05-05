81468d5e4dbabee64f2b4828c0441900e1e4d700
Alperen Ulukaya <alperenulukaya07@gmail.com>
2025-05-05 13:08:17 +0300

    updated customer profile component

--- ecommerce-app/src/app/features/customer/customer.module.ts
+++ ecommerce-app/src/app/features/customer/customer.module.ts
@@ -1,32 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CustomerRoutingModule } from './customer-routing.module'; 
-import { SharedModule } from '../../shared/shared.module'; 
-
+import { CustomerRoutingModule } from './customer-routing.module';
+import { SharedModule } from '../../shared/shared.module';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CustomerHomeComponent } from './pages/customer-home/customer-home.component';
 import { CustomerCartComponent } from './pages/customer-cart/customer-cart.component';
 import { OrderHistoryComponent } from './pages/order-history/order-history.component';
-import { RouterModule } from '@angular/router';
 import { FavoritesComponent } from './pages/favorites/favorites.component';
 import { CustomerNavbarComponent } from './pages/customer-navbar/customer-navbar.component';
 import { CustomerCategoryProductsComponent } from './pages/customer-category-products/customer-category-products.component';
-import { ReactiveFormsModule } from '@angular/forms';
-
+import { CustomerProfileComponent } from './pages/customer-profile/customer-profile.component';
 @NgModule({
-  declarations: [
-    CustomerHomeComponent,
-    CustomerCartComponent,
-    OrderHistoryComponent,
-    FavoritesComponent,
-    CustomerNavbarComponent,
-    CustomerCategoryProductsComponent
-  ],
-  imports: [
-    CommonModule,
-    RouterModule,
-    CustomerRoutingModule,
-    SharedModule ,
-    ReactiveFormsModule
-  ]
+declarations: [
+CustomerHomeComponent,
+CustomerCartComponent,
+OrderHistoryComponent,
+FavoritesComponent,
+CustomerNavbarComponent,
+CustomerCategoryProductsComponent,
+CustomerProfileComponent
+],
+imports: [
+CommonModule,
+RouterModule,
+CustomerRoutingModule,
+SharedModule,
+ReactiveFormsModule
+]
 })
 export class CustomerModule { }
--- ecommerce-app/src/app/features/customer/pages/customer-profile/customer-profile.component.ts
+++ ecommerce-app/src/app/features/customer/pages/customer-profile/customer-profile.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-customer-profile',
   standalone: false,
   templateUrl: './customer-profile.component.html',
-  styleUrl: './customer-profile.component.scss'
+  styleUrls: ['./customer-profile.component.scss']
 })
-export class CustomerProfileComponent {
+export class CustomerProfileComponent implements OnInit {
+  user = {
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    joinedDate: new Date('2022-01-01')
+  };
 
+  constructor() { }
+
+  ngOnInit(): void {}
 }
