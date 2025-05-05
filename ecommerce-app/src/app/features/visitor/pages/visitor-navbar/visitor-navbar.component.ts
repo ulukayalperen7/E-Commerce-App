edab83b401dcd8def55b5cca90ca00f07bd0c1b9
Alperen Ulukaya <alperenulukaya07@gmail.com>
2025-05-05 15:29:17 +0300

    updated visitor navbar

--- ecommerce-app/src/app/features/visitor/pages/visitor-navbar/visitor-navbar.component.ts
+++ ecommerce-app/src/app/features/visitor/pages/visitor-navbar/visitor-navbar.component.ts
@@ -3,27 +3,29 @@ import { Category, CategoryService } from '../../../../core/services/category.se
 
 @Component({
   selector: 'app-visitor-navbar',
-  standalone: false,
+  standalone:false,
   templateUrl: './visitor-navbar.component.html',
   styleUrls: ['./visitor-navbar.component.scss']
 })
 export class VisitorNavbarComponent implements OnInit {
   categories: Category[] = [];
+  categoriesOpen = false;
   isNavbarHidden = false;
   navbarHeight = 0;
+  private lastScrollTop = 0;
 
   constructor(private categoryService: CategoryService) {}
 
   ngOnInit() {
     this.categoryService.getAll().subscribe(cats => this.categories = cats);
-
     this.updateNavbarHeight();
   }
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    const scrollY = window.scrollY || document.documentElement.scrollTop;
-    this.isNavbarHidden = scrollY > 100;
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+    this.isNavbarHidden = st > this.lastScrollTop && st > this.navbarHeight;
+    this.lastScrollTop = st <= 0 ? 0 : st;
   }
 
   @HostListener('window:resize', [])
@@ -37,7 +39,6 @@ export class VisitorNavbarComponent implements OnInit {
   }
 
   toggleCategories() {
-    const menu = document.querySelector('.categories-menu');
-    menu?.classList.toggle('open');
+    this.categoriesOpen = !this.categoriesOpen;
   }
 }
